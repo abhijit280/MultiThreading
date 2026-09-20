915c04d928ded0a377b8dd5f718f40fc5aaff450
abhijit280 <abhijittapu65615@gmail.com>
2026-09-20 14:19:27 +0530

    hello

---
+++ src/com/thread1/day2/Driver1.java
@@ -0,0 +1,22 @@
+package com.thread1.day2;
+ class Process implements Runnable{
+
+	@Override
+	public synchronized void run() {
+		System.out.println("Process.run(>>>>>>>>>>>>>>)"+Thread.currentThread().getName());
+		
+	}
+	 
+ }
+public class Driver1 {
+
+	public static void main(String[] args) {
+		 Process p1 = new Process();
+		 Process p2 = new Process();
+		 Thread t1 = new Thread(p1);
+		 t1.start();
+		 Thread t2 = new Thread(p2);
+		 t2.start();
+	}
+
+}
