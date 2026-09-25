50b57124c3f915457d81d9b6e3654d229497908b
abhijit280 <abhijittapu65615@gmail.com>
2026-09-25 17:06:52 +0530

    hello

--- src/com/thread1/day6/Driver1.java
+++ src/com/thread1/day6/Driver1.java
@@ -2,7 +2,9 @@ package com.thread1.day6;
 
 class Do{
 	public void Dosomething() {
-		System.out.println("Do.Dosomething(>>>>>>>>>>>>>)");
+//		Thread.currentThread().setPriority(10);
+		Thread.yield();
+		System.out.println("Do.Dosomething(>>>>>>>>>>>>>) [ "+Thread.currentThread().getPriority()+" ]");
 	}
 }
 public class Driver1 {
@@ -10,5 +12,11 @@ public static void main(String[] args) {
 	Do do1 = new Do();
 	Thread t1 = new Thread(()-> do1.Dosomething());
 	t1.start();
+	Thread t2 = new Thread(()-> do1.Dosomething());
+	t2.start();
+	t2.setPriority(10);
+	Thread t3 = new Thread(()-> do1.Dosomething());
+	t3.start();
+	
 }
 }
